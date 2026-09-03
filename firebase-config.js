0735b79746100445fbf32bf052cfa9346277275e
akatsukydeliverysushi-coder <akatsuky.deliverysushi@gmail.com>
2026-09-03 11:41:42 -0300

    Configurar conexão do LMC com Firebase

---
+++ firebase-config.js
@@ -0,0 +1,4 @@
+// Configuração pública do Firebase do LMC Combustíveis.
+// Nunca coloque service account, private key ou credenciais administrativas neste arquivo.
+const firebaseConfig={apiKey:'AIzaSyD5UzOeO8wUwNOH4Gvuq6DM_s_hXY5OgaI',authDomain:'lmc-postos.firebaseapp.com',databaseURL:'https://lmc-postos-default-rtdb.firebaseio.com',projectId:'lmc-postos',storageBucket:'lmc-postos.firebasestorage.app',messagingSenderId:'738600686620',appId:'1:738600686620:web:f8012a1df9806f46abbbd9',measurementId:'G-59J5J48V4W'};
+window.LMC_FIREBASE_CONFIG=firebaseConfig;
